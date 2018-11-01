91e87c7f86233deeb8a1ec9e5e954a032f416323
thaibalong7 <thai.balong.7@gmail.com>
2018-11-01 22:46:42 +0700

    ! change CSS for Home screen

--- src/components/presentationals/Home.js
+++ src/components/presentationals/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import '../../App.css';
+import '../../css/Home.css';
 import { connect } from 'react-redux'
 
 
@@ -7,29 +7,38 @@ class Home extends Component {
     click(){
         console.log('click');
     }
+    logoutClick = () =>{
+        this.props.SignOut();
+    }
     render() {
         console.log(this.props)
         return (
             <div className="container clearfix">
-                <div>  <button onClick={this.props.SignOut}>
-                    Sign Out
-			</button></div>
+                <div className="navigation" onClick={this.logoutClick}>
+                    <span className="buttonLogout">
+                        <img src={this.props.userProfile.photoURL} alt="avatar"/>
+                        <div className="logout">LOGOUT</div>
+                    </span>
+                </div>
+
                 <div className="people-list" id="people-list">
                     <div className="search">
                         <input type="text" placeholder="search" />
                         <i className="fa fa-search"></i>
                     </div>
                     <ul className="list">
-                        <li onClick={this.click}>         
-                            <img className="split left" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/chat_avatar_01.jpg" alt="avatar" />
-                            <div className="split right">
-                                <div className="name">Vincent Porter</div>
-                                <div className="status">
-                                    <i className="fa fa-circle online"></i> online
+                        <li onClick={this.click}>
+                            <div className="people-item">         
+                                <img className="split left" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/chat_avatar_01.jpg" alt="avatar" />
+                                <div className="split right">
+                                    <div className="name">Vincent Porter</div>
+                                    <div className="status">
+                                        <i className="fa fa-circle online"></i> online
+                                    </div>
                                 </div>
                             </div>
                         </li>
-                        <li onClick={this.click}>         
+                        <li onClick={this.click} className="people-item">         
                             <img className="split left" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/chat_avatar_05.jpg" alt="avatar" />
                             <div className="split right">
                                 <div className="name">Ginger</div>
@@ -38,7 +47,7 @@ class Home extends Component {
                                 </div>
                             </div>
                         </li>
-                        <li onClick={this.click}>         
+                        <li onClick={this.click} className="people-item">         
                             <img className="split left" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/chat_avatar_07.jpg" alt="avatar"/>
                             <div className="split right">
                                 <div className="name">Christian Kelly</div>
